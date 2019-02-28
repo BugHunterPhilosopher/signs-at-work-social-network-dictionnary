99b0cafc40cc0f621d333d31f8bd8386015844e6
Alien@lv426 <goupilpierre@gmail.com>
2019-02-28 20:39:24 +0100

    /video is a GET in new Dailymotion API

--- app/src/main/java/com/orange/signsatwork/biz/webservice/controller/FileUploadRestController.java
+++ app/src/main/java/com/orange/signsatwork/biz/webservice/controller/FileUploadRestController.java
@@ -209,7 +209,7 @@ public class FileUploadRestController {
 
       HttpEntity<MultiValueMap<String, Object>> requestEntity1 = new HttpEntity<MultiValueMap<String, Object>>(body, headers1);
       ResponseEntity<VideoDailyMotion> response1 = restTemplate1.exchange("https://api.dailymotion.com/videos",
-        HttpMethod.POST, requestEntity1, VideoDailyMotion.class);
+        HttpMethod.GET, requestEntity1, VideoDailyMotion.class);
       VideoDailyMotion videoDailyMotion = response1.getBody();
 
 

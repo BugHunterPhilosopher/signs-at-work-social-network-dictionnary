ef4ff7567c835fe1b265cc333cc066913b2de639
Alien@lv426 <goupilpierre@gmail.com>
2019-03-14 22:16:50 +0100

    Youtube is back

--- app/src/main/java/com/orange/signsatwork/biz/storage/YoutubeAuthService.java
+++ app/src/main/java/com/orange/signsatwork/biz/storage/YoutubeAuthService.java
@@ -5,6 +5,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sqladmin.SQLAdminScopes;
+import com.google.api.services.youtube.YouTubeScopes;
 import com.orange.signsatwork.YoutubeAccess;
 
 import java.io.File;
@@ -26,7 +27,7 @@ public class YoutubeAuthService {
       .setJsonFactory(new JacksonFactory())
       .setServiceAccountId(youtubeAccess.accountId)
       .setServiceAccountPrivateKeyFromP12File(new File(youtubeAccess.privateKey))
-      .setServiceAccountScopes(Collections.singleton(SQLAdminScopes.SQLSERVICE_ADMIN))
+      .setServiceAccountScopes(Collections.singleton(YouTubeScopes.YOUTUBE_UPLOAD))
       .setServiceAccountUser(youtubeAccess.accountId)
       .build();
 

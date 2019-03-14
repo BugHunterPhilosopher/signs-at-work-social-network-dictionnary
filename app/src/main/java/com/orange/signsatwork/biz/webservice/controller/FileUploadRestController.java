88dcf08fec41a41e6f30ab8ec5728382714ed217
Alien@lv426 <goupilpierre@gmail.com>
2019-03-14 22:10:37 +0100

    Youtube is back

--- app/src/main/java/com/orange/signsatwork/biz/webservice/controller/FileUploadRestController.java
+++ app/src/main/java/com/orange/signsatwork/biz/webservice/controller/FileUploadRestController.java
@@ -163,7 +163,7 @@ public class FileUploadRestController {
 
     try {
       // Youtube
-      UploadVideoToYoutubeService.uploadToYoutube(fileOutput);
+      UploadVideoToYoutubeService.uploadToYoutube(fileOutput, appProfile.youtubeAccess());
 
       // Dailymotion
       String dailymotionId;

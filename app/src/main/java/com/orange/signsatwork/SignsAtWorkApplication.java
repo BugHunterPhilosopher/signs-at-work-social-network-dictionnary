b2658ab43365972a1ad8b8152d2ac0526fbcb71d
Alien@lv426 <goupilpierre@gmail.com>
2019-11-24 19:31:01 +0100

    CHANGED en l10n now contains FR strings

--- app/src/main/java/com/orange/signsatwork/SignsAtWorkApplication.java
+++ app/src/main/java/com/orange/signsatwork/SignsAtWorkApplication.java
@@ -23,18 +23,18 @@ package com.orange.signsatwork;
  */
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
@@ -46,6 +46,18 @@ import java.util.concurrent.TimeUnit;
 @PropertySource("classpath:youtube.properties")
 @PropertySource("classpath:google.properties")
 public class SignsAtWorkApplication extends WebMvcConfigurerAdapter {
+
+  @Autowired
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
 
   @Bean
   /** Use the user's browser preferred language for translation */
